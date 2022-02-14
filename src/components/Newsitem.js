1a1036a2c0f56f89bff12953d2c837afde887def
SUMANTA KUMAR DAS <48908320+SUMANTA5@users.noreply.github.com>
2022-02-14 13:34:47 +0530

    api

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -4,15 +4,15 @@ export class Newsitem extends Component {
 
 
   render() {
-    let{title, description, urlToImage, newsUrl} = this.props;
+    let{title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className='my-3'>
           <div className="card" style={{width: "18rem"}}>
-              <img src={urlToImage} className="card-img-top" alt="..."/>
+              <img src={!imageUrl?"https://g.foolcdn.com/editorial/images/664882/1-ford-mach-e-suv.jpeg":imageUrl} className="card-img-top" alt="..."/>
               <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>
