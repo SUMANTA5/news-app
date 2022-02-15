1b344411e4b86b99f9dd6d8c7c25761f706963aa
SUMANTA KUMAR DAS <48908320+SUMANTA5@users.noreply.github.com>
2022-02-15 19:48:05 +0530

    fix title

--- src/components/News.js
+++ src/components/News.js
@@ -15,13 +15,18 @@ export class News extends Component {
     category: PropTypes.string,
   };
 
-  constructor() {
-    super();
+   capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     };
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - News`
   }
 
   async updateNews() {
@@ -53,7 +58,7 @@ export class News extends Component {
     return (
       <div className="container my-3">
         <h1 className="text-center" style={{ margin: "35px 0px" }}>
-          News - Tot Headlines
+          News - Tot {this.capitalizeFirstLetter(this.props.category)} Headlines
         </h1>
         {this.state.loading && <Spinner />}
         <div className="row">
